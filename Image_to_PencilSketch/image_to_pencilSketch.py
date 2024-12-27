cc1e9655d423457756ceb9c64a8d9a9fa632c5c1
Anuja Mahadev Korake <anujakorake@gmail.com>
2024-12-27 21:59:40 +0530

    Initial Commit

--- Image_to_PencilSketch/image_to_pencilSketch.py
+++ Image_to_PencilSketch/image_to_pencilSketch.py
@@ -1,9 +1,9 @@
 import cv2
-image = cv2.imread('MSDhoni.jpeg')
+image = cv2.imread('Flower_image.jpeg')
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 inverted = 255-grayImage
 blur = cv2.GaussianBlur(inverted, (21,21), 0)
 invertedblur = 255-blur
 sketch = cv2.divide(grayImage, invertedblur, scale=256.0)
-cv2.imwrite("skech_image.png", sketch)
+cv2.imwrite("skech1_image.png", sketch)
 cv2.imshow("Image", sketch)
